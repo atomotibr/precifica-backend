f069a27d40fa3d1b484fe15d9313d7ad123a4000
atomotibr <djbrunobr@gmail.com>
2026-04-26 16:37:07 -0300

    precifica

---
+++ server.js
@@ -0,0 +1,34 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const { Pool } = require("pg");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL
+});
+
+app.get("/", (req, res) => {
+  res.send("Precifica API rodando 🚀");
+});
+
+app.get("/produtos", async (req, res) => {
+  const result = await pool.query("SELECT * FROM produtos");
+  res.json(result.rows);
+});
+
+app.post("/produtos", async (req, res) => {
+  const { nome, categoria, preco_loja } = req.body;
+
+  await pool.query(
+    "INSERT INTO produtos(nome, categoria, preco_loja) VALUES($1,$2,$3)",
+    [nome, categoria, preco_loja]
+  );
+
+  res.send("Produto criado");
+});
+
+app.listen(3000, () => console.log("Servidor rodando na porta 3000"));
