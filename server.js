fae703c5301c18e35fa16212da8f8723ce5b4d8a
atomotibr <djbrunobr@gmail.com>
2026-04-26 18:09:03 -0300

    teste sem banco

--- server.js
+++ server.js
@@ -1,49 +1,14 @@
-require("dotenv").config();
-
 const express = require("express");
-const cors = require("cors");
-const { Pool } = require("pg");
 
 const app = express();
-app.use(cors());
-app.use(express.json());
-
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false
-  }
-});
 
 app.get("/", (req, res) => {
-  res.send("Precifica API rodando 🚀");
+  res.send("API FUNCIONANDO SEM BANCO 🚀");
 });
 
-app.get("/teste", async (req, res) => {
-  try {
-    const result = await pool.query("SELECT 1");
-    res.json({ ok: true });
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ erro: err.message });
-  }
-});
-
-app.post("/produtos", async (req, res) => {
-  try {
-    const { nome, categoria, preco_loja } = req.body;
-
-    await pool.query(
-      "INSERT INTO produtos (nome, categoria, preco_loja) VALUES ($1, $2, $3)",
-      [nome, categoria, preco_loja]
-    );
-
-    res.send("Produto criado");
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ erro: err.message });
-  }
+app.get("/teste", (req, res) => {
+  res.json({ ok: true });
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log("Servidor rodando na porta", PORT));
+app.listen(PORT, () => console.log("Rodando na porta", PORT));
